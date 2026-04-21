1ddc52ef3d94cc9e31c4e6031255a3656bc7c343
Niraj <Niraj.gambhava@gmail.com>
2026-04-21 16:25:44 +0530

    feat: add demotable model and update leads table serialId to be nullable

---
+++ prisma/migrations/20260421103733_create_demo_table/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE `leads` MODIFY `serialId` INTEGER NULL AUTO_INCREMENT;
+
+-- CreateTable
+CREATE TABLE `DemoTable` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
